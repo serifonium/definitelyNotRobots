cdc9a7ec7c274735abb404c17aa80bb5b579d95f
jackj <jack.julius@connect.qut.edu.au>
2026-03-24 14:14:53 +1000

    hi? - jack

--- src/main/java/com/example/definitelynotrobots/HelloController.java
+++ src/main/java/com/example/definitelynotrobots/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.definitelynotrobots;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,4 +12,11 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+
+    public void onJackButtonClick() {
+        welcomeText.setText("Hi Im Jack");
+    }
+
 }
