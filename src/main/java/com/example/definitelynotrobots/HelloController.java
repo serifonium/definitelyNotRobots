1d0ae5c7234fbcc8638e057ded2c17e210500b21
jackj <jack.julius@connect.qut.edu.au>
2026-03-24 14:34:00 +1000

    made a jack button 2.0

--- src/main/java/com/example/definitelynotrobots/HelloController.java
+++ src/main/java/com/example/definitelynotrobots/HelloController.java
@@ -14,6 +14,7 @@ public class HelloController {
 
     public void onJackButtonClick() {
         welcomeText.setText("Hi Im Jack");
+        //note
     }
 
 }
--- src/main/java/com/example/definitelynotrobots/Launcher.java
+++ src/main/java/com/example/definitelynotrobots/Launcher.java
@@ -2,7 +2,7 @@ package com.example.definitelynotrobots;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class    Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
