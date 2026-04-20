13d9c2a48282e9700f540bb5449ae98b87cd80ff
Mohamed <n12029548@qut.edu.au>
2026-04-21 00:30:00 +1000

    Updated the MainController UI

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,10 +15,26 @@ import java.io.IOException;
 public class MainController {
 
     @FXML
-    public Label testLabel;
+    public Label testLabel; //This label prints the "Welcome (user)!" text
+    public VBox SavedMeals; //This Vbox holds the list of saved meals.
 
     public void initialize() {
-        testLabel.setText("Welcome, " + UserAccountDAO.currentAccount.getUsername() + "!");
+        SetName();
+        SetSavedMeals();
+    }
+    public void SetName()
+    {
+        testLabel.setText("Welcome, " + UserAccountDAO.currentAccount.getUsername() + "!"); //prints the welcome text
+    }
+    public void SetSavedMeals()
+    { //TODO LATER: Change how this works so it actually prints out saved meals in the database.
+        for (int i = 0; i < 8; i++) { //currently just makes a set of empty boxes
+            VBox meal = new VBox();
+            meal.setStyle("-fx-background-color: #1e8648; -fx-padding: 10;");
+            meal.getChildren().add(new Label("Item " + i));
+
+            SavedMeals.getChildren().add(meal);
+        }
     }
 
     public void goToGroceryList() throws IOException {
