c0e39689c9c62e782eb2c5fd23753578e8bdb9af
Lenard De Leon <lenarddeleon122006@gmail.com>
2026-04-21 14:39:13 +1000

    sign up

--- src/main/java/com/example/definitelynotrobots/HelloApplication.java
+++ src/main/java/com/example/definitelynotrobots/HelloApplication.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     public static final String TITLE = "Not A Chef";
-    public static int WIDTH = 720;
-    public static int HEIGHT = 480;
+    public static int WIDTH = 920;
+    public static int HEIGHT = 1080;
     // 320, 240
 
     @Override
