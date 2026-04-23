4be998629cadc7ff80859e5c84d9f7c078a8cc69
Maya <n11966785@qut.edu.au>
2026-04-23 12:26:06 +1000

    adding ai view to navigation bar

--- src/main/java/controllers/SavedRecipesController.java
+++ src/main/java/controllers/SavedRecipesController.java
@@ -1,4 +1,41 @@
 package controllers;
 
-public class savedRecipes {
+import com.example.definitelynotrobots.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SavedRecipesController {
+    @FXML
+    public Button homeButton;
+
+    public Button userButton;
+    public Button aiButton;
+    public Button pantryButton;
+    public Button groceryListButton;
+    public Button savedRecipesButton;
+
+    public void goToHomeView() throws IOException {
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+    public void goToProfileView() throws IOException{
+        Stage stage = (Stage) userButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("preferences-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+
+    public void saveRecipe() throws IOException{
+
+    }
+
+
+
 }
