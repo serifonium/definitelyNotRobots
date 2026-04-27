3afeeb89b89e93648b906050142ca15b8c10eb58
Mohamed <n12029548@qut.edu.au>
2026-04-27 20:28:33 +1000

    Updated start() in HelloApplication

--- src/main/java/com/example/definitelynotrobots/HelloApplication.java
+++ src/main/java/com/example/definitelynotrobots/HelloApplication.java
@@ -12,6 +12,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        UserAccountDAO userAccountDAO = new UserAccountDAO();
+        userAccountDAO.dropTable();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(TITLE);
