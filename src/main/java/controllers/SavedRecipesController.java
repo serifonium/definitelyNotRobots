6a73dfb5c0ca8eecdd9fa6e1568e11c6a9d64bbe
Maya <n11966785@qut.edu.au>
2026-04-30 14:38:28 +1000

    fixing recipe view buttons

--- src/main/java/controllers/SavedRecipesController.java
+++ src/main/java/controllers/SavedRecipesController.java
@@ -74,6 +74,13 @@ public class SavedRecipesController {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
     }
+    public void goToProfile() throws IOException{
+        Stage stage = (Stage) profileButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Profile-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+
 
     @FXML
     private void onSaveRecipe() {
