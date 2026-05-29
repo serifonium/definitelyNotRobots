44d100a9a008a1300f21bb53b759830bb122c9df
Maya <n11966785@qut.edu.au>
2026-05-29 10:36:47 +1000

    Saved Recipes list size changed to fit on screen

--- src/main/java/com/example/definitelynotrobots/SavedRecipesDAO.java
+++ src/main/java/com/example/definitelynotrobots/SavedRecipesDAO.java
@@ -63,46 +63,6 @@ public class SavedRecipesDAO {
     }
 
 
-    public void deleteSavedRecipe(String recipeTitle) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM savedRecipes WHERE recipeTitle = ?");
-            preparedStatement.setString(1, recipeTitle);
-            preparedStatement.execute();
-        } catch (SQLException ex) {
-            System.err.println(ex);
-        }
-    }
-
-    public List<Recipe> getAllSavedRecipes() {
-        List<Recipe> recipes = new ArrayList<>();
-
-        try {
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM savedRecipes");
-
-            while (rs.next()) {
-                Recipe recipe = new Recipe();
-
-                recipe.setUserAccountIDRecipe(rs.getInt("userAccountIDRecipe"));
-                recipe.setRecipeTitle(rs.getString("recipeTitle"));
-                recipe.setPrepTime(rs.getInt("prepTime"));
-                recipe.setCookTime(rs.getInt("cookTime"));
-                recipe.setServings(rs.getInt("servings"));
-                recipe.setIsSaved(rs.getBoolean("isSaved"));
-                recipe.setIngredients(rs.getString("ingredients"));
-                recipe.setMethod(rs.getString("method"));
-
-                recipes.add(recipe);
-            }
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return recipes;
-    }
-
     public List<Recipe> getSavedRecipesForUser(int userId) {
         List<Recipe> recipes = new ArrayList<>();
 
@@ -132,32 +92,6 @@ public class SavedRecipesDAO {
         }
 
         return recipes;
-    }
-
-    public Recipe getByTitle(String title) {
-        try {
-            PreparedStatement ps = connection.prepareStatement(
-                    "SELECT * FROM savedRecipes WHERE recipeTitle = ?"
-            );
-            ps.setString(1, title);
-
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return new Recipe(
-                        rs.getInt("userAccountIDRecipe"),
-                        rs.getString("recipeTitle"),
-                        rs.getInt("prepTime"),
-                        rs.getInt("cookTime"),
-                        rs.getInt("servings"),
-                        rs.getBoolean("isSaved"),
-                        rs.getString("ingredients"),
-                        rs.getString("method")
-                );
-            }
-        } catch (SQLException ex) {
-            System.err.println(ex);
-        }
-        return null;
     }
 
 
