c59684160db1e2396e972dd3f8c8dede3a792746
Mohamed <n12029548@qut.edu.au>
2026-06-03 22:51:23 +1000

    git test: no actual changes

--- src/main/java/controllers/ProfileController.java
+++ src/main/java/controllers/ProfileController.java
@@ -58,5 +58,4 @@ public class ProfileController extends BaseController {
 //    public void goToPreferences() throws IOException { goToPage("preferences-view"); }
 //    public void goToFitnessTargets() throws IOException { goToPage("fitness-targets-view"); }
 //    public void goToSavedRecipeView() throws IOException { goToPage("saved-recipes-view"); }
-
 }
