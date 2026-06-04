7af81a99302982e601c3d54c5f3ce9264075bf7f
Mohamed <n12029548@qut.edu.au>
2026-06-05 03:09:01 +1000

    Removed recipe scroll wheel with recent recipe section

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.example.definitelynotrobots.HelloApplication;
+import com.example.definitelynotrobots.Recipe;
 import com.example.definitelynotrobots.UserAccountDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +19,6 @@ public class MainController {
     @FXML
     public void initialize() throws IOException {
         SetName();
-        SetSavedMeals();
         ScaleMainView(1.6);
     }
 
@@ -29,35 +29,14 @@ public class MainController {
         testLabel.setText("Welcome, " + UserAccountDAO.currentAccount.getFirstname() + "!"); //prints the welcome text
     }
 
-    public VBox SavedMeals; //This Vbox holds the list of saved meals.
-
-    public void SetSavedMeals() throws IOException {
-        //TODO LATER: Change how this works so it actually prints out saved meals in the database.
-        SavedMeals.getChildren().clear();
-
-        for (int i = 0; i < 8; i++) {
-
-            FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/com/example/definitelynotrobots/meal-template.fxml")
-            );
-
-            HBox meal = loader.load();
-
-            meal.setStyle("-fx-background-color: #1e8648; -fx-padding: 10;");
-
-            Label label = new Label("Item " + i);
-            meal.getChildren().add(label);
-
-            SavedMeals.getChildren().add(meal);
-        }
-    }
-
     public HBox mainRoot; //This Hbox is the main parent.
 /// TODO LATER: ADD DESCRIPTION for ALL SCALE FUNCTIONS
     private void ScaleMainView(double scale) {
         mainRoot.setScaleX(scale); //Scales root parent
         mainRoot.setScaleY(scale);
     }
+
+
 
     public void goToGroceryList() throws IOException {
         Stage stage = (Stage) testLabel.getScene().getWindow();
